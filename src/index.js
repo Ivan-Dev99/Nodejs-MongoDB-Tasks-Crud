f6c9959b5bf33d429bb6ffb5572c85eec4867905
Ivan Blanco <ovis_54@outlook.com>
2025-03-22 01:24:22 -0500

    deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
-import app from "./app";
-import "./database";
+    import app from "./app";
+    import "./database";
 
-
-app.listen(3000);
-console.log("Server on port", 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
